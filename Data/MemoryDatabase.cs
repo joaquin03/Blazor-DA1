16378bdd7469a9fe1e28dde75e0cd9ac3ccbdd01
joaquin03 <janduano@gmail.com>
2023-01-26 00:19:21 -0300

    add Edit , delate Categories

--- Data/MemoryDatabase.cs
+++ Data/MemoryDatabase.cs
@@ -9,12 +9,33 @@ public class MemoryDatabase{
 
     public MemoryDatabase()
     {
-        this.AddMovies();
-        this.AddCategories();
-    
+        this.AddDefaultMovies();
+        this.AddDefaultCategories();
     }
 
-    private void AddMovies()
+    public void AddCategory(Category category)
+    {
+        category.Id = Categories.Count + 1;
+        Categories.Add(category);
+    }
+
+    public Category FindCategory(int id)
+    {
+        return Categories.FirstOrDefault(c => c.Id == id);
+    }
+
+    public void UpdateCategory(Category element)
+    {
+        MemoryDatabase.Categories = MemoryDatabase.Categories.Select(e => e.Id == element.Id ? element : e).ToList();
+    }
+
+    public void DeleteCategory(Category element)
+    {
+        MemoryDatabase.Categories.RemoveAll(x => x.Id == id);
+    }
+
+
+    private void AddDefaultMovies()
     {
         Movies.Add(new Movie { Id = 1, Title = "The Shawshank Redemption", Director = "Frank Darabont", ReleaseYear = 1994 });
         Movies.Add(new Movie { Id = 2, Title = "The Godfather", Director = "Francis Ford Coppola", ReleaseYear = 1972 });
@@ -24,8 +45,7 @@ public class MemoryDatabase{
         Movies.Add(new Movie { Id = 6, Title = "Pulp Fiction", Director = "Quentin Tarantino", ReleaseYear = 1994 });
         Movies.Add(new Movie { Id = 7, Title = "Schindler's List", Director = "Steven Spielberg", ReleaseYear = 1993 });
     }
-
-    private void AddCategories()
+    private void AddDefaultCategories()
     {
         Categories.Add(new Category { Id = 1, Name = "Action" });
         Categories.Add(new Category { Id = 2, Name = "Comedy" });
